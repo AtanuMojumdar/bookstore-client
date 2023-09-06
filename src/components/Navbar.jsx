0dec44666ee3ec6bca4492e628d4c759b8d7356f
Atanu Mojumdar <91253222+AtanuMojumdar@users.noreply.github.com>
2023-09-06 10:05:08 +0530

    modified navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
   })
   return (
     <div className='flex justify-between px-10 w-full mx-auto mt-6'>
-    <Link href='/' className='underline decoration-[#403E3E] font-medium text-gray-900'>BookStore</Link>
+    <Link href='/' className='font-medium text-gray-900'>BookStore</Link>
     {data && <div className="flex gap-16">
     <Link href='/' className='underline decoration-[#403E3E] font-medium text-gray-900'>Categories</Link>
       <Link href='/' className='underline decoration-[#403E3E] font-medium text-gray-900'>Best Seller</Link>
@@ -23,8 +23,8 @@ const Navbar = () => {
     </div>}
     {data && <div className="flex gap-2">
       <Link href='/profile' className='underline decoration-[#403E3E] font-medium text-gray-900'>Profile</Link></div>}
-    {!data && <> <Link href='/login' className='underline decoration-[#403E3E] font-medium text-gray-900'>Login</Link>
-    <Link href='/signup' className='underline decoration-[#403E3E] font-medium text-gray-900'>Signup</Link> </>}
+    {!data && <div className="flex gap-8"> <Link href='/login' className='underline decoration-[#403E3E] font-medium text-gray-900'>Login</Link>
+    <Link href='/signup' className='underline decoration-[#403E3E] font-medium text-gray-900'>Signup</Link> </div>}
     </div>
   )
 }
